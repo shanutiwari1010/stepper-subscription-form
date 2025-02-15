29d45bb62159867e0e5a46c3a3bb20815e5ed979
Shanu Tiwari <shanutiwari.work@gmail.com>
2025-02-16 00:30:23 +0530

    css update

--- src/pages/subscription-form.tsx
+++ src/pages/subscription-form.tsx
@@ -3,7 +3,7 @@ import FormLayout from "@/components/subscription/form-layout";
 
 export default function SubscriptionForm() {
   return (
-    <div className="min-h-screen bg-red-500 flex items-center justify-center p-4 w-full">
+    <div className="min-h-screen flex items-center justify-center p-4 w-full">
       <SubscriptionProvider>
         <FormLayout />
       </SubscriptionProvider>
