7e94dce07bd7866aff99d12dcda8f7b3a3c2f556
Shanu Tiwari <shanutiwari.work@gmail.com>
2025-02-15 13:47:55 +0530

    style : added padding

--- src/components/subscription/step-sidebar.tsx
+++ src/components/subscription/step-sidebar.tsx
@@ -11,16 +11,17 @@ export default function StepSidebar() {
   const { currentStep } = useSubscription();
 
   return (
-    <aside className="flex md:block justify-center gap-4 md:gap-8 p-8 bg-[url('https://raw.githubusercontent.com/bradtraversy/react-step-form/main/src/assets/images/bg-sidebar-desktop.svg')] bg-no-repeat bg-cover rounded-lg text-white">
+    <aside className="flex md:block justify-center gap-4 md:gap-8 p-8 bg-[url('/assets/images/bg-sidebar-desktop.svg')] bg-no-repeat bg-cover rounded-lg text-white">
+
       {steps.map((step) => (
         <div key={step.number} className="flex items-center gap-4">
-          <div className={`w-8 h-8 rounded-full border-2 flex items-center justify-center font-bold
+          <div className={`w-8 h-8 rounded-full border-2 flex items-center justify-center font-bold mb-3
             ${currentStep === step.number ? 'bg-[hsl(206,94%,87%)] text-black border-[hsl(206,94%,87%)]' : 'border-white'}`}>
             {step.number}
           </div>
           <div className="hidden md:block">
             <p className="text-sm text-gray-300">{step.subtitle}</p>
-            <p className="font-bold">{step.title}</p>
+            <p className="font-bold text-sm mb-5">{step.title}</p>
           </div>
         </div>
       ))}
